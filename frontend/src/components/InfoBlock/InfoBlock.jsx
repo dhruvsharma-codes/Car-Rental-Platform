4e61bbf01db5efa64e58ab1f8aa54a368473688d
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-10 13:38:43 +0530

    updates

--- frontend/src/components/InfoBlock/InfoBlock.jsx
+++ frontend/src/components/InfoBlock/InfoBlock.jsx
@@ -3,38 +3,71 @@ const InfoBlock = () => {
   const info = [
     {
       id: 1,
-      icon: <MapPin width={50} height={50} color="black" />,
+      icon: <MapPin size={50} color="black" />,
       title: "Availability",
       description:
         "Diam tincidunt tincidunt erat at semper fermentum. Id ultricies quis",
     },
     {
       id: 2,
-      icon: <CarIcon width={60} height={60} color="black" />,
+      icon: <CarIcon size={50} color="black" />,
       title: "Comfort",
       description:
         "Gravida auctor fermentum morbi vulputate ac egestas orci etiam convallis",
     },
     {
       id: 3,
-      icon: <Wallet width={50} height={50} color="black" />,
+      icon: <Wallet size={50} color="black" />,
       title: "Savings",
       description:
         "Pretium convallis id diam sed commodo vestibulum lobortis volutpat",
     },
   ];
   return (
-    <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-      <div className="flex flex-wrap items-center lg:justify-between md:justify-center md:gap-6 sm:justify-center gap-10  py-13">
+    <section
+      className="
+        w-full max-w-7xl
+        mx-auto px-4
+        sm:px-6
+        lg:px-8
+      "
+    >
+      <div
+        className="
+          flex flex-wrap
+          py-13
+          items-center justify-center gap-10
+          lg:justify-between
+        "
+      >
         {info.map((details) => (
-          <div key={details.id}>
-            <div className="flex flex-col gap-3 justify-center items-center">
+          <div
+            key={details.id}
+            className="
+              w-full max-w-[320px]
+            "
+          >
+            <div
+              className="
+                flex flex-col
+                gap-3 justify-center items-center
+              "
+            >
               {details.icon}
-              <h3 className="text-2xl font-secondary font-semibold text-black">
+              <h3
+                className="
+                  text-2xl font-secondary font-semibold text-black
+                "
+              >
                 {details.title}
               </h3>
             </div>
-            <p className="mt-5 max-w-80 text-center text-[16px] font-normal leading-6 text-black">
+            <p
+              className="
+                mt-5
+                text-center text-[16px] font-normal leading-6 text-black
+              "
+            >
               {details.description}
             </p>
           </div>
