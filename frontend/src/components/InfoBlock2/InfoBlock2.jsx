c8ea63c3fb23da39f3315433287f3059a6cad66c
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-10 15:55:29 +0530

    updates

--- frontend/src/components/InfoBlock2/InfoBlock2.jsx
+++ frontend/src/components/InfoBlock2/InfoBlock2.jsx
@@ -28,7 +28,7 @@ const InfoBlock2 = () => {
   ];
   return (
     <section className=" w-full">
-      <div className=" max-w-7xl mx-auto  px-4 sm:px-6 lg:px-6 flex   items-center gap-27.5 min-h-157 ">
+      <div className=" max-w-7xl mx-auto w-full flex-col px-4 sm:px-6 lg:flex-row lg:gap-20 py-16 lg:px-8 lg:py-24 flex items-center gap-8 min-h-157 ">
         {/* left image */}
         <img src={info_img} alt="info-image" className=" h-130 ml-20" />
 
