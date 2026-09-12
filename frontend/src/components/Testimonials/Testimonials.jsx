fd07f4795b1d62326e3806cd4eec3bf0c9b94f53
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-12 14:45:11 +0530

    updates

--- frontend/src/components/Testimonials/Testimonials.jsx
+++ frontend/src/components/Testimonials/Testimonials.jsx
@@ -1,18 +1,8 @@
-
-const Testimonials = ({
-  id,
-  name,
-  image,
-  description,
-  profile,
-  company,
-}) => {
+const Testimonials = ({ id, name, image, description, profile, company }) => {
   return (
     <div className="relative w-full">
-
       {/* Review */}
       <div className="relative flex min-h-75 items-center rounded-t-[20px] bg-[#F9F9F9] px-5 py-10 sm:min-h-80 sm:px-6">
-
         <img
           className="absolute left-6 top-6 h-auto w-10 sm:left-8 sm:top-8 sm:w-12"
           src={image}
@@ -22,12 +12,10 @@ const Testimonials = ({
         <p className="w-full max-w-85 text-[17px] font-medium leading-7 sm:text-[18px] lg:text-[20px]">
           {description}
         </p>
-
       </div>
 
       {/* Profile */}
       <div className="relative rounded-b-[20px] bg-[#5937E0] px-5 pb-6 pt-14 sm:px-6">
-
         <img
           className="absolute left-6 -top-7.5 h-16 w-16 rounded-full object-cover sm:left-8"
           src={profile}
@@ -43,7 +31,6 @@ const Testimonials = ({
             {name}
           </h4>
         </div>
-
       </div>
     </div>
   );
